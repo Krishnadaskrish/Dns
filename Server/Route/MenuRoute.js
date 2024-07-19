5c2ccaaefa55f68cc3e3fe89aa5f5dcf6777b124
hornto <krishnadas10.official@gmail.com>
2024-07-19 23:33:11 +0530

    change

--- Server/Route/MenuRoute.js
+++ Server/Route/MenuRoute.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const MenuRoute = express.Router()
-import MenuController from '../controllers/MenuController.js';
-
+import MenuController from '../controllers/MenuController.js'
 
 
 MenuRoute
